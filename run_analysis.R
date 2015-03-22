471f8f639137ec31c4af030d0cffbe4311c974de
Maxim Fedorenko <varlllog@gmail.com>
2015-03-22 04:12:46 +0300

    [R] Added the get_tidy function and the tidy.txt file.

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,58 @@
 #!/usr/bin/env R CMD BATCH
-run_analysis <- function () {
-    activities <- read.table("UCI HAR Dataset/activity_labels.txt",
+get_dataset <- function (datadir="UCI HAR Dataset") {
+    activities <- read.table(paste0(datadir, "/activity_labels.txt"),
                              col.names = c("activity_id", "activity_label"))
-    features <- read.table("UCI HAR Dataset/features.txt",
+    features <- read.table(paste0(datadir, "/features.txt"),
                            col.names = c("feature_id", "feature_label"))
     features.needed <- features[grep("(mean|std)\\(\\)", features$feature_label),]
 
-    f <- data.frame();
+    res <- data.frame();
     sapply(c("test", "train"), function(set) {
-        x <- read.table(paste0("UCI HAR Dataset/", set, "/X_", set, ".txt"),
+        x <- read.table(paste0(datadir, "/", set, "/X_", set, ".txt"),
                         col.names = features$feature_label)
         x <- x[,features.needed$feature_id]
 
-        y <- read.table(paste0("UCI HAR Dataset/", set, "/y_", set, ".txt"),
+        y <- read.table(paste0(datadir, "/", set, "/y_", set, ".txt"),
                         col.names = c("activity_id"))
         y <- data.frame(activities$activity_label[unlist(y)])
         names(y) = c("activity_label")
 
-        subj <- read.table(paste0("UCI HAR Dataset/", set, "/subject_", set, ".txt"),
+        subj <- read.table(paste0(datadir, "/", set, "/subject_", set, ".txt"),
                            col.names = c("subj_id"))
 
-        x <- cbind(y, x)
-        x <- cbind(subj, x)
+        x <- cbind(subj, y, x)
 
-        f <<- rbind(f,x)
-    });
+        res <<- rbind(res,x)
+    })
 
-    return (f)
+    return(res)
 }
-#run_analysis();
+
+get_tidy <- function (dataset=data.frame()) {
+    by.subj <- split(dataset, dataset$subj_id)
+    res <- data.frame()
+    sapply(by.subj, function(x) {
+        by.activity <- split(x, x$activity_label)
+        sapply(by.activity, function(y) {
+            subj <- unique(y$subj_id)
+            activity <- unique(y$activity_label)
+
+            col <- data.frame(subj, activity)
+
+            for(i in 3:length(names(y))) { col <- cbind(col, mean(y[,i])) }
+
+            res <<- rbind(res, col)
+        })
+    })
+    names(res) = names(dataset)
+
+    return(res)
+}
+
+run_analysis <- function (datadir="UCI HAR Dataset", file="tidy.txt") {
+    dataset <- get_dataset(datadir)
+    tidy <- get_tidy(dataset)
+    return(tidy)
+}
+
+run_analysis();
